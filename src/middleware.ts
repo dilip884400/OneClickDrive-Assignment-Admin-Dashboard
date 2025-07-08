3954426575168ba3e6164d749dd2a9ebb36e32e6
dilip.884400 <dilip.884400@gmail.com>
2025-07-09 00:45:47 +0530

    by default login route

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,6 +5,10 @@ export function middleware(request: NextRequest) {
   const isLoggedIn = request.cookies.get('admin')?.value
   const isLoginPage = request.nextUrl.pathname === '/login'
 
+  if (request.nextUrl.pathname == "/") {
+    return NextResponse.redirect(new URL('/login', request.url))
+  }
+
   if (!isLoggedIn && !isLoginPage) {
     return NextResponse.redirect(new URL('/login', request.url))
   }
@@ -17,5 +21,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/dashboard', '/edit/:path*', '/login'],
+  matcher: ['/dashboard', '/login', '/'],
 }
